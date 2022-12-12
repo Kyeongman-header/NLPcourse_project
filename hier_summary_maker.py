955e04341938307e48c0649ee1bc96228eb0b6b3
Kyeongman-header <zzangman2@gmail.com>
2022-12-12 21:43:11 +0900

    first

---
+++ hier_summary_maker.py
@@ -0,0 +1,418 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import os
+import numpy as np
+from transformers import pipeline
+from transformers import AutoTokenizer, TFAutoModelForSeq2SeqLM
+from summarizer import Summarizer
+from tqdm import tqdm,trange
+import torch
+import csv
+import sys
+import ctypes as ct
+import math
+csv.field_size_limit(int(ct.c_ulong(-1).value // 2))
+
+#  Returns a bool indicating if CUDA is currently available.
+# print(torch.cuda.is_available())
+# print(torch.cuda.device_count())
+# torch.cuda.device(0)
+
+def createFolder(directory):
+        try:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+        except OSError:
+            print('Error Creating directory. ' + directory)
+
+
+# tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer.pad_token=tokenizer.eos_token
+tokenizer.pad_token_id=tokenizer.eos_token_id
+
+def hier_summary_maker(START=0,RANGE=10,report=False, is_abs_or_ext=False, seq_length=100,file="train",is_model_or_given_dataset=True,device=0):
+    if is_abs_or_ext :
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn",device=1)
+    else :
+        summarizer = Summarizer()
+
+    
+
+    #total_source=[]
+    
+    total_target=[]
+    f = open('reedsy_wp.csv', 'r', encoding='utf-8')
+    #f = open('reedsy_wp.csv', 'r',encoding='utf-8')
+    
+    rdr = csv.reader(f)
+    
+    for line in rdr:
+        #print(line)
+        #total_target.append(print(line[1]))
+        #total_source.append(line[1])
+        total_target.append(line[1])
+        #input()
+    
+
+    #f.close()
+    #print(len(total_target))
+    
+    
+    #T=file
+    #if "train" in file:
+    #    T="train"
+    #elif "valid" in file:
+    #    T="valid"
+    
+    """
+    total_source=[]
+    with open("writingPrompts/"+ T +".wp_source", encoding='UTF8') as f:
+       stories = f.readlines()
+       stories = [" ".join(i.split()[0:1000]) for i in stories]
+       temp_stories=[]
+       for story in stories:
+           temp_stories.append(story.replace("<newline>",""))
+       total_source.append(temp_stories)
+
+    total_target=[]
+
+    with open("writingPrompts/"+ T +".wp_target", encoding='UTF8') as f:
+       stories = f.readlines()
+       stories = [" ".join(i.split()[0:1000]) for i in stories]
+       temp_stories=[]
+       for story in stories:
+           temp_stories.append(story.replace("<newline>",""))
+       total_target.append(temp_stories)
+    """
+
+    #if RANGE != 0:
+    #    whole_data=total_target[0][START:START+RANGE]
+    #else:
+    #    whole_data=total_target[0]
+    if RANGE !=0:
+        whole_data=total_target[START:START+RANGE]
+    else:
+        whole_data=total_target
+
+    token_len=[]
+    seq_len=[]
+    word_len=[]
+
+    middle_token_len=[]
+    middle_seq_len=[]
+    middle_word_len=[]
+    mother_token_len=[]
+    mother_seq_len=[]
+    mother_word_len=[]
+
+    #whole_target=total_target[:RANGE]
+    #whole_source=total_source[:RANGE]
+
+    #print(len(whole_target[0]))
+    #print(whole_target[0])
+    #print(len(whole_target[1]))
+    #print(whole_target[1])
+    middle_summary_prefix_target=[]
+    final_summary_prefix_target=[]
+    whole_summary_set=[]
+    whole_target_set=[]
+    real_summary_set=[]
+    middle_summary_set=[]
+    middle_target_set=[]
+
+    c=0
+    seq_length=0
+
+    print("len : " + str(len(whole_data)))
+
+    for t in tqdm(whole_data):
+            # t에서 ...은 다 없애야
+            #input()
+            #print(t)
+            ppp=len(tokenizer(t).input_ids)
+            if(ppp<2000):
+                print("too short story. " + str(ppp))
+                continue
+            print("//////****************************//////")
+            #print(t)
+            #print()
+
+            c=c+1
+            continue_flag=False
+            t=t.replace("......",".")
+            t=t.replace(".....",".")
+            t=t.replace("....",".")
+            t=t.replace("...",".")
+            t=t.replace("..",".")
+            t=t.replace(";",";.")
+            t=t.replace(":",":.")
+            t=t.replace("!","!.")
+            t=t.replace("?","?.")
+            t=t.replace('“'," ")
+            t=t.replace('”'," ")
+            t=t.replace('"'," ")
+            t=t.replace("*","")
+            #t=t.replace("\'"," ")
+            
+            if seq_length<=0:
+               tt=t
+            else:
+               tt=('.').join(t.split('.')[:seq_length])
+            # seq length로 조절하는 것은 무의미하고,
+            # 그 전에 words 개수로 '자른다'
+
+            # tt=(' ').join(t.split(' '))
+        
+            if len(tt)==0:
+                continue
+            
+            #print(tt)
+            #print('len: '+str(len(tt)))
+
+            tl=len(tokenizer(tt).input_ids)
+            sl=len(tt.split('.'))
+            wl=len(tt.split(' '))
+            seq_len.append(sl)
+            token_len.append(tl)
+            word_len.append(wl)
+            #print("---$$---")
+            #print("sl: " + str(sl))
+            #print("wl: "+str(wl))
+            #print("tl: "+str(tl))
+
+            ### 최대 1024 토큰 이하가 되도록 덩어리 나누기.
+            seq=[0]
+            l=0
+            count=0
+            Words=int(round(tl/5))
+            #print("Words: " + str(Words))
+
+            for split in tt.split('.'):
+                count=count+1
+                if len(seq)>=6:
+                    # 5 덩어리에서 끊는다.
+                    continue
+                tokens=tokenizer(split).input_ids
+                l=l+len(tokens)
+                if l>=Words:
+                    seq.append(count)
+                    l=len(tokens)
+                
+            
+            seq.append(count)
+            ### seq엔 나눠진 덩이리들(문장의 인덱스)가 있음
+            
+            #print("tl: " + str(tl))
+            #print("count : " + str(count))
+            if len(seq)>6:#6덩어리가 있는 상태.
+                dist=seq[-1]-seq[-2]+1
+                dist=math.ceil(dist/5)
+
+                while seq[5]<sl:
+                    seq[1]+=dist
+                    seq[2]+=dist*2
+                    seq[3]+=dist*3
+                    seq[4]+=dist*4
+                    seq[5]+=dist*5 # 마지막 seq[5]가 실제 len을 넘어도 큰 상관은 없다.
+
+            # else: # 이론상 5개가 무조건 되야 한다. 
+            #     dist=seq[-1]-seq[-2]+1
+            #     dist=math.ceil(dist/5)
+            #     while seq[-1]<sl:
+            #         for s in range(len(seq)):
+            #             if s>0:
+            #                 seq[s]+=dist*s
+
+            #print("seq 배열 : ")
+            #print(seq)
+
+
+            
+            middle_target=[]
+            for i in range(len(seq)):
+                if i>0:
+                    mt=('.').join(tt.split('.')[seq[i-1]:seq[i]])
+                    if(len(tokenizer(mt).input_ids)>1023):
+                        continue_flag=True
+
+                    #print("each middel target token length : " + str(len(tokenizer(mt).input_ids)))
+
+                    middle_target.append(mt)
+            
+            if continue_flag:
+                continue
+            #print("middle target length : "+str(len(middle_target)))
+            
+            mt_summary=[]
+            middle_summary=""
+            if len(middle_target) > 5: # 구조상 나오면 안되지만, 혹시 모르니까.
+                print("middle target length is over than 5")
+                continue
+
+            for mt in middle_target:
+                token_mt=tokenizer(mt).input_ids
+                middle_token_len.append(len(token_mt))
+                middle_seq_len.append(len(mt.split('.')))
+                middle_word_len.append(len(mt.split(' ')))
+                    #print("mt len : "  + str(len(tokenizer(mt).input_ids)))
+                
+                if len(token_mt)>1000 or len(token_mt)<200:
+                    print("too short final target.")
+                    continue_flag=True
+                    break
+                s=summarizer(mt) # 이 min length는 token 개수가 아니라 글자수라서 토큰 개수는 훨씬 훨씬 작다
+                    #print(s)
+                    #print(len(tokenizer(s).input_ids))
+                if is_abs_or_ext :
+                    s=s[0]["summary_text"]
+                    
+                middle_summary=middle_summary+" "+s
+
+                mt_summary.append(s)
+            
+            if continue_flag:
+                continue
+
+            print("middle sumary : " + middle_summary)
+            print("len middel summary : " + str(len(tokenizer(middle_summary).input_ids)))
+            if len(tokenizer(middle_summary).input_ids)>1000 or len(tokenizer(middle_summary).input_ids)<200:                
+                print("too short middle summary.")
+                continue
+            
+            #print("middle_summary len: " + str(len(tokenizer(middle_summary).input_ids)))
+                
+            mother_plot=summarizer(middle_summary)
+
+            if is_abs_or_ext :
+                mother_plot=mother_plot[0]["summary_text"]
+                
+            middle_summary_prefix_target.append("The most abstract summary is : " + mother_plot + " and the plot is : " + middle_summary) 
+                # GPT 학습을 위한 PREFIX 데이터셋을 생성한다.
+
+            mother_token_len.append(len(tokenizer(mother_plot).input_ids))
+            mother_seq_len.append(len(mother_plot.split('.')))
+            mother_word_len.append(len(mother_plot.split(' ')))
+            
+            
+            print("mother plot : " + mother_plot)
+            print("len mother plot : " + str(len(tokenizer(mother_plot).input_ids)))
+
+            for i in range(len(middle_target)):
+                final_summary_prefix_target.append("The most abstract summary is : " + mother_plot + " and the plot is : " + mt_summary[i] + " and the plot's sequence is " + str(i) + " and the original text is : " + middle_target[i])
+            # GPT 학습을 위한 prefix 데이터셋을 생성한다.
+
+            # middle_target-> original target이 분할됨, mt_summary->분할된 미들 서머리들, middle_summary-> middle summary 통째, 
+            # mother_plot -> 최종 서머리, 가 전부 준비되었으므로, 데이터셋을 통째로 만든다.
+            # middel_summary가 1차적인 decoder input이 된다.
+            # mother_plot이 1차적인 encoder input이 된다.
+            # mt_summary가 각각 2차적인 encoder input이 된다.
+            # middle_target이 각각 2차적인 decoder input이 된다.
+
+            #if len(seq)<=10:
+            #    for i in range(11-len(seq)):
+            #        mt_summary.append("<pad>")
+            #        middle_target.append("<pad>")
+                    # 강제로 10개가 되도록 맞춰준다.
+            
+            whole_summary_set.append(mother_plot)
+            #whole_target_set.append(middle_summary)
+            #middle_summary_set.append(mt_summary[:10]) # 아마 10개를 넘는 녀석은 없겠지만, 혹시 모르니 해준다.
+            #middle_target_set.append(middle_target[:10])
+            #real_summary_set.append(whole_source[c-1])
+            # 차원을 통일해야 한다
+            # 어차피 저장하는건 token이다. 
+            # whole summary set, whole target set은 예전에 하듯이 토큰 만들면  된다. (size, 1024)와 (size, 100)이 나올거다
+            # middle summary set은 기본적으로 [["~","~",...], ["~",~~] ] 이런 구조이다. 타겟도 마찬가지.
+            # 얘네도 차원을 통일해 줘야 한다. 내부적으로 차원이 10개씩으로 반드시 맞추고,
+            # 최종적으로 (size, 10, 100)이랑 (size,10,1024)가 나와야 한다.
+            # 당연하지만 각각의 셋은 10개를 못넘는 애들도 있을 거다(10개를 넘는 애는 없을듯) 이런 애들은 "<pad>" 배열이라도 넣어야 한다.
+
+
+
+    middle_summary_prefix_target=tokenizer(middle_summary_prefix_target,return_tensors="tf",padding="max_length",max_length=1024,truncation=True).input_ids
+    final_summary_prefix_target=tokenizer(final_summary_prefix_target,return_tensors="tf",padding="max_length",max_length=1024,truncation=True).input_ids
+    npmiddle_summary_prefix_target=middle_summary_prefix_target.numpy()
+    npfinal_summary_prefix_target=final_summary_prefix_target.numpy()
+
+    print(npmiddle_summary_prefix_target.shape)
+    print(npfinal_summary_prefix_target.shape)
+
+    #whole_summary_set=tokenizer(whole_summary_set,return_tensors="tf",padding="max_length",max_length=200, truncation=True).input_ids
+    #whole_target_set=tokenizer(whole_target_set,return_tensors="tf",padding="max_length",max_length=1024,truncation=True).input_ids
+    #real_summary_set=tokenizer(real_summary_set,return_tensors="tf",padding="max_length",max_length=200,truncation=True).input_ids
+    #mss=[]
+    #for m in middle_summary_set:
+    #    mss.append(tokenizer(m,return_tensors="tf",padding="max_length",max_length=200, truncation=True).input_ids)
+    #mts=[]
+    #for m in middle_target_set:
+    #    mts.append(tokenizer(m,return_tensors="tf",padding="max_length",max_length=1024, truncation=True).input_ids)
+
+    #npwhole_summary_set=whole_summary_set.numpy()
+    npwhole_summary_set=np.array(whole_summary_set)
+    #npwhole_target_set=whole_target_set.numpy()
+    #npmts=np.array(mts)
+    #npmss=np.array(mss)
+    #npreal_whole_summary_set=real_summary_set.numpy()
+    #print(npwhole_summary_set.shape)
+    #print(npwhole_target_set.shape)
+    #print(npreal_whole_summary_set.shape)
+    #print(npmss.shape)
+    #print(npmts.shape)
+
+    #file="TOR"
+
+    createFolder("npdata/"+file)
+    print(os.path.isfile("./npdata/"+file+"/mother.npy"))
+    if os.path.isfile("./npdata/"+file+"/mother.npy"):
+        past=np.load("./npdata/"+file+"/mother.npy")
+        npwhole_summary_set=np.concatenate((past,npwhole_summary_set),axis=0)
+    np.save("./npdata/"+file +"/mother",npwhole_summary_set)
+
+    if os.path.isfile("./npdata/"+file+"/middle_tokens.npy"):
+        past=np.load("./npdata/"+file+"/middle_tokens.npy")
+        npmiddle_summary_prefix_target=np.concatenate((past,npmiddle_summary_prefix_target),axis=0)
+    np.save("./npdata/"+file +"/middle_tokens",npmiddle_summary_prefix_target)
+
+    if os.path.isfile("./npdata/"+file+"/final_tokens.npy"):
+        past=np.load("./npdata/"+file+"/final_tokens.npy")
+        npfinal_summary_prefix_target=np.concatenate((past,npfinal_summary_prefix_target),axis=0)
+    np.save("./npdata/"+file +"/final_tokens",npfinal_summary_prefix_target)
+    #np.save("./npdata/"+file +"/middle_target",npmts)
+    #np.save("./npdata/"+file +"/real_mother",npreal_whole_summary_set)
+
+    a=np.array(seq_len)
+    b=np.array(token_len)
+    c=np.array(word_len)
+    d=np.array(middle_token_len)
+    e=np.array(middle_seq_len)
+    f=np.array(middle_word_len)
+    h=np.array(mother_token_len)
+    i=np.array(mother_seq_len)
+    j=np.array(mother_word_len)
+    def analyze(a,name):
+        print("-----$$------")
+        print("data name : " + name)
+        print("mean : " + str(np.mean(a)))
+        print("variance : " +str(np.var(a)))
+        print("std : " + str(np.std(a)))
+        print("median : " + str(np.median(a)))
+        print("bincount : " + str(np.bincount(a).argmax()))
+        print("min : " + str(np.min(a)))
+        print("1 quantile : " + str(np.quantile(a,0.25)))
+        print("2 quantile : " + str(np.quantile(a,0.5)))
+        print("3 quantile : " + str(np.quantile(a,0.75)))
+        print("max : " + str(np.max(a)))
+    if report :
+        analyze(a,"whole seq_len")
+        analyze(b,"whole token_len")
+        analyze(c,"whole word_len")
+        analyze(e,"middle_seq_len")
+        analyze(d,"middle_token_len")
+        analyze(f,"middle_word_len")
+        analyze(i,"mother_seq_len")
+        analyze(h,"mother_token_len")
+        analyze(j,"mother_word_len")
+
+hier_summary_maker(0,0,report=True,is_abs_or_ext=True,file="reedsy_wp")
---
+++ hier_utility.py
@@ -0,0 +1,288 @@
+
+from rouge import Rouge
+import tqdm
+import tensorflow as tf
+rouge=Rouge()
+
+def prefix_ver1(val_sum):
+    
+    # print(val_sum)
+    return "The most abstract summary is : " + val_sum + " and the plot is : "
+    
+def prefix_ver2(val_sum,mother_plot, i):
+    
+    return "The most abstract summary is : " + mother_plot + " and the plot is : " + val_sum + " and the plot's sequence is " + str(i) + " and the original text is : "
+    
+
+def generate_valid(model,valid_summary,wr,epoch,tokenizer,val_inp,prefix,mother_plots=[]):
+    c=0
+    wr.writerow(['this is ' + str(epoch) + 'epoch generation task.'])
+    r_1_avg=0
+    r_2_avg=0
+    r_l_avg=0
+    outputs=[]
+    for val_sum in valid_summary:
+
+        
+        
+        if len(mother_plots)!= 0:
+            
+            mother_plot=mother_plots[int(math.floor(c/5))]
+        # 이거로 해야 함.
+
+        # 여기 이 tokenizer 부분은 지워야함
+            #mother_plot=tokenizer.decode(mother_plots[int(math.floor(c/5))],skip_special_tokens=True)
+            val_sum=prefix(val_sum,mother_plot,c%5)
+        # c가 0~4까지는 c/5는 0이다. 5~9까지 c/5는 1이다. 이렇게 mother_plot을 5개씩 매칭한다.
+            #페이지는 0~4까지 범위를 가진다
+        
+        else :
+            #val_sum=tokenizer.decode(val_sum,skip_special_tokens=True)
+        # 잘못 만들었음... 여기 tokenizer 부분은 지워야 함
+            val_sum=prefix(val_sum)
+        
+        print("val summary ===> " + val_sum) # prefix 부분이다.
+        # ~~~!! 적절한 prefix를 붙여줘야 한다!!
+        input_ids = tokenizer.encode(val_sum, return_tensors='tf')
+        # max length가 1024지, 실제로 1024개를 생성하는 경우는 없을 거다.
+        
+        output=""
+        while len(output)==0: # length가 매우 드물게 0인 경우가 있는데 이럴 경우 다시 생성한다.
+            output = tokenizer.decode(model.generate(input_ids,max_length = 1000,do_sample=True,top_p=0.92,top_k=50,early_stopping=True)[0],skip_special_tokens=True) # no repeat은 dumb repeat을 방지할 수도 있다!
+        
+        
+        original=tokenizer.decode(val_inp[c],skip_special_tokens=True)
+        
+        # ~~~!! prefix는 없애줘야 한다!
+        val_sum=val_sum.replace(" ,",",")
+        val_sum=val_sum.replace(" .",".")
+        val_sum=val_sum.replace(" '","'")
+        val_sum=val_sum.replace(' "','"')
+        val_sum=val_sum.replace(" n't","n't")
+        val_sum=val_sum.replace(u'\xa0',u'')
+        origianl=original.replace(" ,",",")
+        original=original.replace(" .",".")
+        original=original.replace(" '",".")
+        original=original.replace(' "','"')
+        original=original.replace(" n't","n't")
+        original=original.replace(u'\xa0',u'')
+        output=output.replace(" ,",",")
+        output=output.replace(" .",".")
+        output=output.replace(" '","'")
+        output=output.replace(' "','"')
+        output=output.replace(" n't","n't")
+        output=output.replace(u'\xa0',u'')
+
+        
+        #print("val_sum : "+val_sum)
+        print("is val_sum exist : " )
+        print(output.find(val_sum)>=0) # False가 나오면 뭔가 이상한 거다
+
+
+        output=output.replace(val_sum,"")
+        
+        original=original.replace(val_sum,"") # prefix 부분은 없애준다.
+        print("original : " + original)
+        print("len of original : " + str(len(tokenizer(original).input_ids)))
+        print()
+        print("output : " + output)
+        print("len of output : " + str(len(tokenizer(output).input_ids)))
+        
+        outputs.append(output) # prefix부분은 빠진 output.
+
+        if len(output)==0:
+            r_1=0
+            r_2=0
+            r_l=0
+        else:
+            r=rouge.get_scores(output,original,avg=True)
+            #bleu=sentence_bleu([original],output)
+            r_1=r['rouge-1']['r']
+            r_2=r['rouge-2']['r']
+            r_l=r['rouge-l']['r'] # recall이 맞을 것 같다.(reference 길이가 분모가 됨.)
+        
+        r_1_avg=r_1_avg+r_1
+        r_2_avg=r_2_avg+r_2
+        r_l_avg=r_l_avg+r_l
+        c=c+1
+        print(r_1)
+        print(r_2)
+        print(r_l)
+        wr.writerow([original,val_sum,output,r_1,r_2,r_l])
+    # 되는 것 확인.
+    
+
+
+    #encodings=tf.reshape(valid_summary,[1,-1]) # 지우고 아래 주석으로 해야함
+    
+    # 여기서부턴 한꺼번에 perplexity 계산.
+    #encodings=tokenizer("\n\n".join(valid_summary),return_tensors="tf")
+    val_inp=tokenizer.batch_decode(val_inp,skip_special_tokens=True)
+    encodings=tokenizer("\n\n".join(val_inp),return_tensors="tf").input_ids
+    print("encoding input shape : " )
+    #print(encodings.input_ids.shape) # (1,287664가 됨!! 여러 글이 한 덩어리로 합쳐짐.)
+    
+    print(encodings.shape) # 지우고 위의 주석으로 해야함
+
+    max_length = model.config.n_positions
+    stride = 512
+    #size=encodings.input_ids.shape[1] 
+    size=encodings.shape[1] # 지우고 위의 주석으로 해야함
+    nlls = []
+    print("model's output max_length : " + str(max_length))
+    #print("input id size : " + str(encodings.input_ids.shape[1])) 
+    
+    print("input id size : " + str(encodings.shape[1])) # 지우고 위의 주석으로 해야함
+    
+    print("stride : " + str(stride))
+    prev_end_loc=0
+
+    # input()
+
+    for begin_loc in (range(0, size, stride)): # (0~287644)를 512씩 움직인다. 이 경우 
+        end_loc = min(begin_loc + max_length, size) 
+                # print("begin_loc : " + str(begin_loc))
+                # print("end_loc : " + str(end_loc))
+
+
+        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+        if trg_len==0:
+            continue
+        input_ids = encodings.input_ids[:, begin_loc:end_loc] # begin loc은 512씩 가는데 end_loc은 거기서부터 1024까지의 길이이기때문에
+        
+        #input_ids = encodings[:, begin_loc:end_loc] # 지우고 위의 주석으로 해야함
+
+
+                # 0~1024, 512~1536, 1024~2048 이런식으로 input은 겹치면서 나아간다
+                # 고로 end에서 1024만큼 뺀 부분은 -100으로 target을 설정해 줌으로써 그부분의 loss는 계산에서 제외할 수 있대(어째서지...)
+                # 그니까, 앞의 512개의 context도 전부 보면서, 뒤의 512개의 생성에 대한 perplexity를 계산하겠다는 거지. 
+                # 그 와중에 앞의 512개의 perplexity를 또 계산하면 중복되니깐 그 부분은 -100으로 설정해서 없애겠다는거고.
+                # 이 stride가 작을 수록 perplexity는 작아짐(좋아짐.) 왜냐하면 겹치면서 보는 context가 많아지니까?
+                # print("input_ids : ") # 2차원임[[]]
+                # print(input_ids.shape)
+                # print("trg_len : " + str(trg_len))
+
+                # target_ids[:, :-trg_len] = -100
+        py_target=[]
+        for i in range(input_ids.shape[1]):
+            if i<input_ids.shape[1]-trg_len:
+                py_target.append(-100)
+            else:
+                py_target.append(input_ids[0][i])
+
+
+        target_ids=tf.stack([py_target])
+
+                # print("target_ids : ")
+                # print(target_ids.shape) # [[]]
+                # input()
+
+        loss = model(input_ids, labels=target_ids).loss
+                # print("outputs : ")
+                # print(outputs[0])
+                # print(outputs.loss)
+        neg_log_likelihood = loss * trg_len
+
+                # print("neg_log_likelihood : ")
+                # print(neg_log_likelihood)
+        prev_end_loc = end_loc
+        nlls.append(neg_log_likelihood)
+
+    ppl = tf.exp(tf.reduce_sum(tf.stack(nlls)) / end_loc)
+
+    print("ppl is : " + str(ppl))
+
+    
+    wr.writerow(['this is avg rouge of generation: ' + str(r_1_avg/c) + " =r1 , " + str(r_2_avg/c) + " =r2 , " + str(r_l_avg/c) + " =rl , "])
+    wr.writerow(['this is avg perplexity of generation : ' + str(ppl)])   
+    return r_1_avg/c , r_2_avg/c ,r_l_avg/c , ppl, outputs
+
+
+import math
+def splitting_output(outputs,tokenizer):
+    five_split_outputs=[]
+    for o in outputs:
+        w=round(len(tokenizer(o).input_ids)/5)
+        sl=len(o.split('.'))
+        seq=[0]
+        count=0
+        l=0
+        for split in o.split('.'):
+            count=count+1
+            if len(seq)>=6:
+                # 5 덩어리에서 끊는다.
+                continue
+            tokens=tokenizer(split).input_ids
+            l=l+len(tokens)
+            if l>=w:
+                seq.append(count)
+                l=len(tokens)
+        seq.append(count)
+        
+        #print("before")
+        #print(seq)
+        """ 
+        if len(seq)>6:#6덩어리가 있는 상태.
+            dist=seq[-1]-seq[-2]+1
+            dist=math.ceil(dist/5)
+
+            while seq[5]<sl:
+                seq[1]+=dist
+                seq[2]+=dist*2
+                seq[3]+=dist*3
+                seq[4]+=dist*4
+                seq[5]+=dist*5
+        """
+        if len(seq)>=7: # 이게 그니까 한 문장의 길이랑 whole/5 의 길이가 비슷할 때 6덩어리가 나오는 것 같다.
+            # 그래서 최소 2000 words 이상에서 작업하는 hier는 6덩어리가 나올 일이 아예 없었던거다.
+            # 그러나 지금은 막 150단어짜리도 나오고 그러다보니깐 5로 나눈게 한 문장 길이랑 거의 비슷한 수준이다.
+            # 그러다보니 끝에 따라지 6번째 덩어리가 나오게 된다.
+            # 긴 output에 대해서는 이러한 현상이 없기 때문에, 간단히 seq[6], 즉 마지막을 seq[5]로 pull한다.
+            seq[5]=seq[6]
+        
+        #print("after")
+        #print(seq)
+        
+        #print("whole : ")
+        #print(o)
+        if len(seq)<6: # 도대체 어떤 이유인지 짐작하기 어렵지만, 만약 5덩어리보다 작게 만들어졌다면,
+            # (아마도 생성된 결과가 몹시 짧을때?? 잘 모르겠다)
+            # 그냥 문장 단위로 자르는 것을 포기하고 단어 단위로 잘라버린다.
+            print("during splitting, the seq length is under 6. words split start.")
+            cut=round(len(o.split(' '))/5)
+            
+            mt=o.split(' ')[0:cut]
+            five_split_outputs.append(mt)
+            print("each middel target token length : " + str(len(tokenizer(mt).input_ids)))
+            print(mt)
+            
+            mt=o.split(' ')[cut: 2* cut]
+            five_split_outputs.append(mt)
+            print("each middel target token length : " + str(len(tokenizer(mt).input_ids)))
+            print(mt)
+            
+            mt=o.split(' ')[2*cut:3*cut]
+            five_split_outputs.append(mt)
+            print("each middel target token length : " + str(len(tokenizer(mt).input_ids)))
+            print(mt)
+
+            mt=o.split(' ')[3*cut:4*cut]
+            five_split_outputs.append(mt)
+            print("each middel target token length : " + str(len(tokenizer(mt).input_ids)))
+            print(mt)
+
+            mt=o.split(' ')[4*cut:]
+            five_split_outputs.append(mt)
+            print("each middel target token length : " + str(len(tokenizer(mt).input_ids)))
+            print(mt)
+
+            return five_split_outputs
+
+        for i in range(6): # 무조건 5개씩만 한다(어떤 연유로, 예를 들어 너무 짧은 output이었다거나, 해서 이상한 오류가 나도 5덩어리만 본다)
+            if i>0:
+                mt=('.').join(o.split('.')[seq[i-1]:seq[i]])
+                mt=mt+"."
+                print("each middel target token length : " + str(len(tokenizer(mt).input_ids)))
+                print(mt)
+                five_split_outputs.append(mt)
+    return five_split_outputs
